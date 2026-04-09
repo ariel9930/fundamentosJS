4e7482b8ee7fdba807e105dab3dc9e60395e56ae
ariel9930 <arielfaundez@liceovvh.cl>
2026-04-09 12:57:57 -0400

    d
    d

---
+++ 01_introduccion/hola_mundo/static/js/script.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo, desde archivo externo!...");
+
+
+// 1) Hola mundo con variable y const.
+const saludo = "hola a todos, ";
+let destinatario = "Estudiantes";
+// Impresion de saludo concatenando con (+)
+console.log(saludo + "para los" + destinatario)
+
+// 2) Saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre: ");
+console.log("hola, " + nombre);
---
+++ 02_tipo_de_datos/1.-tipo_de_texto/static/js/javascript.js
@@ -0,0 +1,10 @@
+console.log("conexion con js correcta...");
+/*
+Dato: Tipo texto (string)
+Concanetacion de texto con el signo +
+Podemos unir textos y variables
+*/
+const nombre = "Ariel";
+const apellido = "Faundez";
+// Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
