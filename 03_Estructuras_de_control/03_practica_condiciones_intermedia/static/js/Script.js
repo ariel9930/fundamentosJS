65a52e996b988f52d8b99bfe031ac399796f778c
ariel9930 <arielfaundez@liceovvh.cl>
2026-05-04 09:17:30 -0400

    c
    c

--- 03_Estructuras_de_control/03_practica_condiciones_intermedia/static/js/Script.js
+++ 03_Estructuras_de_control/03_practica_condiciones_intermedia/static/js/Script.js
@@ -6,7 +6,6 @@ console.log("Conexion exitosa con JS...")
 function ejercicio11() {
 let nota = 6.7;
 let nota1 = "ariel"
-
 if(nota >= 6.0){
     alert(` El estudiante ${nota1} tiene nota ${nota}
         \ny su resultado es: Exelente rendimiento!`);
@@ -16,7 +15,6 @@ if(nota >= 6.0){
     alert(`Estudiante reprobado`);
 }
 }
-
 
 // EJERCICIO 12
 // Resultado esperado en alert:
@@ -126,3 +124,118 @@ if (nota>=6.0) {
 } 
 }
 
+// Ejercicio 19: Clasificación de desempeño con más niveles
+// Crear una función que almacene el nombre de un estudiante y su promedio final. Luego:
+
+// Convertir el nombre a mayúsculas
+// Clasificar el rendimiento según:
+// ≥ 6.5 → Sobresaliente
+// ≥ 6.0 → Muy buen rendimiento
+// ≥ 5.0 → Buen rendimiento
+// ≥ 4.0 → Suficiente
+// < 4.0 → Insuficiente
+
+// Además:
+
+// Mostrar la cantidad de caracteres del nombre
+function ejercicio19(){
+let nombre = "tetetesahur"
+let promedio = 6.7
+
+if (promedio>=6.5) {
+        alert(`El nombre del estudiante ${nombre.toUpperCase()} tiene ${nombre.length} caracteres y saco un promedio de ${promedio}
+    \npor lo cual es sobresaliente`);
+} else  if (promedio>=6.0) {
+alert(`El nombre del estudiante ${nombre.toUpperCase()} tiene ${nombre.length} caracteres y saco un promedio de ${promedio}
+    \npor lo cual el estudiante tiene un muy buen rendimiento`);
+}
+else if (promedio>=5.0) {
+alert(`El nombre del estudiante ${nombre.toUpperCase()} tiene ${nombre.length} caracteres y saco un promedio de ${promedio}
+    \npor lo cual el estudiante tiene un buen rendimiento`);
+}
+else if (promedio>=4.0) {
+alert(`El nombre del estudiante ${nombre.toUpperCase()} tiene ${nombre.length} caracteres y saco un promedio de ${promedio}
+    \npor lo cual es suficiente`);
+}
+else{
+alert(`El nombre del estudiante ${nombre.toUpperCase()} tiene ${nombre.length} caracteres y saco un promedio de ${promedio}
+    \npor lo cual es insuficiente`);
+}
+}
+
+// Ejercicio 20: Clasificación de producto con múltiples rangos de precio
+// Crear una función que almacene el nombre de un producto y su precio. Luego:
+
+// Convertir el producto a minúsculas
+// Clasificar el precio:
+// ≥ 200.000 → Producto de lujo
+// ≥ 100.000 → Producto premium
+// ≥ 50.000 → Producto estándar
+// ≥ 20.000 → Producto económico
+// < 20.000 → Producto básico
+
+// Además:
+
+// Aplicar descuento:
+// Si es ≥ 100.000 → 15%
+// Si es ≥ 50.000 → 10%
+// Si es menor → sin descuento
+
+// Mostrar:
+
+// Nombre transformado
+// Largo del nombre del producto
+// Precio original
+// Precio final
+// Clasificación
+function ejercicio20(){
+let producto = "TUNGTUNGSAHUR VERSION ORO"
+let precio = 300000
+if (precio>= 200000 ) {
+        alert(`El producto "${producto.toLowerCase()}" con ${producto.length} caracteres
+        \ntiene un precio original de $${precio} pero al ser producto de lujo tiene un precio final de: $${precio * 0.85}`);
+}
+else if (precio>=100000) {
+            alert(`El producto "${producto.toLowerCase()}" con ${producto.length} caracteres
+        \ntiene un precio original de $${precio} pero al ser producto premium tiene un precio final de: $${precio * 0.85}`);
+}
+else if (precio>=50000) {
+            alert(`El producto "${producto.toLowerCase()}" con ${producto.length} caracteres
+        \ntiene un precio original de $${precio} pero al ser producto estandar tiene un precio final de: $${precio * 0.9}`);
+}
+else if (precio>=20000) {
+            alert(`El producto "${producto.toLowerCase()}" con ${producto.length} caracteres
+        \ntiene un precio original de $${precio} pero al ser producto economico tiene un precio final de: $${precio}`);
+}
+else{
+            alert(`El producto "${producto.toLowerCase()}" con ${producto.length} caracteres
+        \ntiene un precio original de $${precio} pero al ser producto basico tiene un precio final de: $${precio}`);
+}
+}
+// Ejercicio 21: Evaluación de frase y puntaje con múltiples condiciones
+// Crear una función que almacene una frase y un puntaje. Luego:
+
+// Convertir la frase a mayúsculas
+// Contar su largo
+// Clasificar el puntaje:
+// ≥ 90 → Excelente
+// ≥ 80 → Muy bueno
+// ≥ 70 → Bueno
+// ≥ 60 → Regular
+// < 60 → Deficiente
+
+// Además:
+
+// Clasificar la frase por tamaño:
+
+// 30 → Muy larga
+
+// 20 → Larga
+
+// 10 → Mediana
+
+// ≤ 10 → Corta
+
+function ejercicio21(){
+
+}
